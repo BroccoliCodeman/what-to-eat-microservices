736083ec2b6e3170f1ea6032cacce7c85ab5ac3a
SteveKSV <amigo.kl08@gmail.com>
2024-03-07 18:39:33 +0200

    Fixed two files for Swagger work
    Fixed Program.cs for Swagger, when in Production environment and changed launchSettings.json -> added Recipe.API.Local (Production env) for working with local database of each team member

--- Recipes.API/Program.cs
+++ Recipes.API/Program.cs
@@ -21,11 +21,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Recipes.API v1"));
-}
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Recipes.API v1"));
+
 
 app.UseAuthorization();
 app.MapControllers();
